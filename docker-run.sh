705c37fcdfe8bf9d861fd4ae0b1e90fbcfa7a4d7
nvietanh1988 <nvietanh1988@gmail.com>
2024-12-12 09:43:43 +0700

    add run docker sh

---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+docker run --rm   -e META_ID="1"   -e META_QRYNAME="DRE01_TEST"   -e RPTNAME="DRE01_TEST_docker"   talend-job-dremio
+docker run --rm   -e META_ID="1"   -e META_QRYNAME="DRE01_TEST"   -e RPTNAME="DRE01_TEST_docker"   talend-job-dremio
+
